d3ffedc657b374a5cca460b629b5d47456440f3e
Emilse Arias <emilsearias@gmail.com>
2025-08-14 21:25:31 -0300

    agregamos la validacion del formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,22 +1,42 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTarea from "./ListaTarea";
+import { useForm } from "react-hook-form";
 
 const FormularioTarea = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const posteriorValidacion = (data) => {
+    console.log(data.tarea)
+    //limpiar el formulario
+    reset()
+  }
+
   return (
     <section>
-      <Form>
-        <Form.Group
-          className="mb-3 d-flex justify-content-between"
-        >
-          <Form.Control type="text" placeholder="Ingresa una tarea" />
+      <Form onSubmit={handleSubmit(posteriorValidacion)}>
+        <Form.Group className="mb-3 d-flex justify-content-between">
+          <Form.Control type="text" placeholder="Ingresa una tarea" {...register('tarea',{
+            required: "La tarea es un dato obligatorio",
+            minLength:{
+                value: 2,
+                message: "La tarea debe tener al menos 2 caracteres"
+            },
+             maxLength:{
+                value: 50,
+                message: "La tarea debe tener como maximo 50 caracteres"
+            }
+          })}/>
           <Button variant="primary" type="submit">
             ➕
           </Button>
         </Form.Group>
-        <Form.Text className="text-danger">
-          Error al cargar una tarea
-        </Form.Text>
+        <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
       </Form>
       <ListaTarea />
     </section>
